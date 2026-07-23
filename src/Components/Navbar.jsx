88a4fffdd2b20aef080b973c47b9ac6b99bf0873
ch4tan <80721563+ch4tan@users.noreply.github.com>
2026-07-23 18:57:14 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,14 +5,17 @@ const Navbar = () => {
     const pathName = window.location.pathname.replace(/[^A-Za-z0-9]/g, "");
     const allTabs = ["Single", "Multiple"];
     const [position, setPosition] = useState(allTabs.includes(pathName) ? pathName : "/"); //default => /
-    const cssNavLink = id => position === id ? "text-white" : "text-black";
+    const cssNavLink = id => position === id ? "text-white font-bold" : "text-black";
 
     return (
-        <nav className="flex bg-gray-600 items-center justify-center gap-8 h-14 text-xl font-bold border-b-2 border-solid border-white">
-            <NavLink to={"/"} onClick={() => setPosition("/")} className={cssNavLink("/")}>About</NavLink>
-            <NavLink to={"/Multiple"}  onClick={() => setPosition("Multiple")} className={cssNavLink("Multiple")}>Multiple Files</NavLink>
-            <NavLink to={"/Single"}  onClick={() => setPosition("Single")} className={cssNavLink("Single")}>Single File</NavLink>
-        </nav>
+        <div className="flex w-full h-14 items-center justify-center">
+
+            <nav className="flex bg-gray-600 items-center justify-center h-7 w-80 rounded-xl border-2 border-solid border-white gap-8">
+                <NavLink to={"/"} onClick={() => setPosition("/")} className={cssNavLink("/")}>About</NavLink>
+                <NavLink to={"/Multiple"}  onClick={() => setPosition("Multiple")} className={cssNavLink("Multiple")}>Multiple Files</NavLink>
+                <NavLink to={"/Single"}  onClick={() => setPosition("Single")} className={cssNavLink("Single")}>Single File</NavLink>
+            </nav>
+        </div>
     );
 };
 
