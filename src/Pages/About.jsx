62b12c337e749f5c11c6a661617d001a8781e082
ch4tan <80721563+ch4tan@users.noreply.github.com>
2026-07-23 19:00:57 +0200

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
             </div>
             <div className={`flex flex-col w-[80vw] text-white text-justify gap-[2vh] md:gap-[5vh] md:text-2xl justify-center items-center`}>
                 <p className="text-center">This web app was designed to simplify the lives of connected athletes.</p>
-                <img src="/public/tcx.svg" alt="Image Not Found" className="w-[10vw] h-[10vh]" />
+                <img src="/tcx.svg" alt="Image Not Found" className="w-[10vw] h-[10vh]" />
                 <p className="text-center">It allows you to instantly generate customized workout files in TCX format, ready to be imported into your favorite platforms (Strava, Garmin, Coros...).</p>
-                <img src="/public/tcx.svg" alt="Image Not Found" className="w-[10vw] h-[10vh]" />
+                <img src="/tcx.svg" alt="Image Not Found" className="w-[10vw] h-[10vh]" />
                 <p className="text-center">Whether you need to fix a missing activity or plan a workout, create your running or cycling sessions in just a few clicks, no fuss.</p>
             </div>
 
