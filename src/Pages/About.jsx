82b1df09f340b5bc7728f2288e9c0607663a947f
ch4tan <80721563+ch4tan@users.noreply.github.com>
2026-07-23 18:57:47 +0200

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,13 +1,17 @@
 const About = () => {
     return (
-        <div className="flex items-center justify-center flex-col gap-12 2xl:justify-between pt-10 h-full">
-            <p className="text-white text-3xl 2xl:text-7xl">TCXATOR</p>
-            <div className={`flex flex-col w-[85%] text-white text-justify text-[100%] 2xl:text-xl gap-2`}>
+        <div className="flex flex-col h-full w-full gap-[5vh] items-center">
+            <div className="flex text-white text-[10vh] text-center justify-center items-end h-[15vh] font-bold">
+                <p className="">TCXATOR</p>
+            </div>
+            <div className={`flex flex-col w-[80vw] text-white text-justify gap-[2vh] md:gap-[5vh] md:text-2xl justify-center items-center`}>
                 <p className="text-center">This web app was designed to simplify the lives of connected athletes.</p>
+                <img src="/public/tcx.svg" alt="Image Not Found" className="w-[10vw] h-[10vh]" />
                 <p className="text-center">It allows you to instantly generate customized workout files in TCX format, ready to be imported into your favorite platforms (Strava, Garmin, Coros...).</p>
+                <img src="/public/tcx.svg" alt="Image Not Found" className="w-[10vw] h-[10vh]" />
                 <p className="text-center">Whether you need to fix a missing activity or plan a workout, create your running or cycling sessions in just a few clicks, no fuss.</p>
             </div>
-            <p className="text-white">Copyright 2026 by TCXATOR - All Rights Reserved</p>
+
         </div>
     );
 };
